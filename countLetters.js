219e302ba81d7f21dec3b5e95f1ee72767b187e9
Daman Bual <damanbual@gmail.com>
2024-05-21 16:27:55 -0700

    added function and test

--- countLetters.js
+++ countLetters.js
@@ -22,3 +22,5 @@ const countLetters = function(sentence) {
   return result;
 };
 
+console.log(countLetters("lighthouse in the house"));
+// output should be { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
