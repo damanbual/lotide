57ada02a1db99317b46866fb2e6f0e4d38f5eb01
Daman Bual <damanbual@gmail.com>
2024-05-22 09:42:31 -0700

    added additional test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -25,3 +25,9 @@ const bestTVShowsByGenre = {
   drama: "The Wire",
 };
 
+// Test cases provided and added
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), undefined);
