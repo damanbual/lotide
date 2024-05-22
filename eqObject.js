b64fd885e00e3c729f77fa77f8abbc5374a2ea6e
Daman Bual <damanbual@gmail.com>
2024-05-22 10:33:54 -0700

    added eqObjects function

--- eqObject.js
+++ eqObject.js
@@ -10,8 +10,26 @@ const assertEqual = function (actual, expected) {
 // Returns true if both objects have itendtical keys with identical values.
 // Otherwise you get back a big fat false!
 
-const eqObjects = function (object1, object2) {};
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
 
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+
+// Test cases
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
