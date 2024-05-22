81796fa83975c22acbb40c4fdd8ee6d0a26298f7
Daman Bual <damanbual@gmail.com>
2024-05-21 20:56:31 -0700

    added more fixes from AI submission feedback

--- letterPositions.js
+++ letterPositions.js
@@ -1,23 +1,12 @@
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 // The eqArrays function
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
+  return arr1.every((element, index) => element === arr2[index]);
 };
+
 // The assertArraysEqual function
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -28,7 +17,7 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 // Function called letterPositions
-const letterPositions = function (sentence) {
+const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
     const letter = sentence[i];
