65c32d02ace60f7310b5af90efdd66fa83ca2fcf
Daman Bual <damanbual@gmail.com>
2024-05-22 09:41:15 -0700

    added Function to FindKeyByValue and copied test case from exercise

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,27 @@
 
+// assertEqual Function
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
+};
+
+// Function to find key by value in an object 
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+// Test cases
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
 };
 
