b9133fcf0771a06ebbb0b930dd481ca2b3c49899
Daman Bual <damanbual@gmail.com>
2024-05-21 20:35:17 -0700

    added assertArraysEqual function and assertEqual function.

---
+++ letterPositions.js
@@ -0,0 +1,28 @@
+
+// assertEqual Funciton
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// assertArraysEqual Function
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  }
+};
+
+
+// Function called letterPositions
+const letterPositions = function (sentence) {
+  const results = {};
+  // logic to update results here
+  return results;
+};
+
+
