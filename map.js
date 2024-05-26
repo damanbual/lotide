142002dba72aa96697f3470d04b9c4a5223893c1
Daman Bual <damanbual@gmail.com>
2024-05-26 13:17:45 -0700

    added implementation of function after purposely casuing undefined output

--- map.js
+++ map.js
@@ -1,2 +1,12 @@
 
 const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function () {
+  const results = [];
+  return results;
+};
+
+
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
