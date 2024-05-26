1812c585b9f4abbdb24113dd5cf8878bc7a8e3de
Daman Bual <damanbual@gmail.com>
2024-05-26 13:28:55 -0700

    added tests

--- map.js
+++ map.js
@@ -10,5 +10,35 @@ const map = function (array, callback) {
 };
 
 
-const results1 = map(words, (word) => word[0]);
-console.log(results1);
+// Added the test arrays
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
+  }
+};
+
+
+// Test 1: 
+const results1 = map(words, (word)=> word[0]);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+// Test 2:
+const results2 = map(words, (word)=> word.toUpperCase());
+assertArraysEqual(results2, ['GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM']);
+
+// Test 3:
+const results3 = map(words, (word)=> word.length);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
