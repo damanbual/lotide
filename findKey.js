bd5e31bb71bdb7bed8e4b74f7c5cf824ba47495d
Daman Bual <damanbual@gmail.com>
2024-05-26 13:53:27 -0700

    added assertEqual tests with help

--- findKey.js
+++ findKey.js
@@ -18,3 +18,18 @@ const assertEqual = function (actual, expected) {
   }
 };
 
+
+// Test cases
+const starsData = {
+  "Blue Hill": { stars: 1 },
+  Akaleri: { stars: 3 },
+  noma: { stars: 2 },
+  elBulli: { stars: 3 },
+  Ora: { stars: 2 },
+  Akelarre: { stars: 3 }
+};
+assertEqual(findKey(starsData, x => x.stars === 2), "noma");
+assertEqual(findKey(starsData, x => x.stars === 3), "Akaleri");
+assertEqual(findKey(starsData, x => x.stars === 1), "Blue Hill");
+assertEqual(findKey(starsData, x => x.stars === 4), undefined);
+
