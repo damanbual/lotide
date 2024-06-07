66bc530da03fe6bc5de8d091e0ff348f3e0da574
Daman Bual <damanbual@gmail.com>
2024-06-07 13:39:29 -0700

    added module exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
 
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${JSON.stringify(actual)} === ${JSON.stringify(expected)}`);
@@ -18,3 +7,4 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${JSON.stringify(actual)} !== ${JSON.stringify(expected)}`);
   }
 };
+module.exports = assertArraysEqual;
