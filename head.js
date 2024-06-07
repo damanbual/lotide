b79abd3af29e49a9fe641835074ca7ad4abd8d96
Daman Bual <damanbual@gmail.com>
2024-06-07 13:16:25 -0700

    removed assertequal function

--- head.js
+++ head.js
@@ -1,13 +1,7 @@
 function head(array) {
   return array[0];
 }
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
+
 // Test cases
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
