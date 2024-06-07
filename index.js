e47fefebd7c1147c1035bd0d6fdd0120ae62063a
Daman Bual <damanbual@gmail.com>
2024-06-07 14:14:44 -0700

    added shorthand property value

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ const middle = require('./middle');
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
-const otherFunction1 = require('./otherFunction1');
-const otherFunction2 = require('./otherFunction2');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  otherFunction1: otherFunction1,
-  otherFunction2: otherFunction2
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertEqual
 };
