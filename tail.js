3d90951cbb34d9615fa48131824788d98c5672c9
Daman Bual <damanbual@gmail.com>
2024-06-07 13:29:37 -0700

    added module export

--- tail.js
+++ tail.js
@@ -1,18 +1,5 @@
-/* eslint-disable func-style */
 
-function assertEqual(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-}
-
-// tail function
-function tail(array) {
+const tail = function(array) {
   return array.slice(1);
-}
-
-// Test
-const result1 = tail([1, 2, 3, 4]);
-assertEqual(result1[0], 2);      // Check first element
+};
+module.exports = tail;
